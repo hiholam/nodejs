9a133ded667415d872c5310e5888680f858c3239
Chan <lynn.chan@accenture.com>
2016-12-23 15:16:47 +0800

    testing

--- test1/server.js
+++ test1/server.js
@@ -1,4 +1,4 @@
-// content of index.js
+/*// content of index.js
 const http = require('http')  
 const port = 3000
 
@@ -15,4 +15,49 @@ server.listen(port, (err) => {
   }
 
   console.log(`server is listening on ${port}`)
+})*/
+
+/*const express = require('express')  
+const app = express()  
+const port = 3000
+
+app.get('/', (request, response) => {  
+  response.send('Hello from Express!')
 })
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})*/
+
+const express = require('express')  
+const app = express()
+
+/*app.use((request, response, next) => {  
+  console.log(request.headers)
+  next()
+})*/
+
+app.use((request, response, next) => {  
+  request.chance = Math.random()
+  next()
+})
+
+app.get('/', (request, response) => {  
+  response.json({
+    chance: request.chance
+  })
+
+  throw new Error('oops')
+})
+
+app.use((err, request, response, next) => {  
+  // log the error, for now just console.log
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(3000)  
